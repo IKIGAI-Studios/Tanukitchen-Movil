6bed729bf29aee5ce83774ec886b2633d6d50350
sowa <106477529+sowa-remon@users.noreply.github.com>
2023-03-18 10:43:36 -0600

    cambio de paquetes
    porque la dependencia dependia de otra dependencia q dependia de otra dependencia q

--- lib/utilities/consts.dart
+++ lib/utilities/consts.dart
@@ -1,4 +1,5 @@
-const CONN = "mongodb://localhost:27017/";
+const CONN =
+    "mongodb+srv://root:root@tanucluster.98dt6wk.mongodb.net/myFirstDatabase";
 // Agregar colecciones
 
 const COLLECTION_USERS = 'user';
